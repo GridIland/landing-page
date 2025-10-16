f99312a840c6e5edff28fc6cdaa73d25110924bd
GridIland <koussougboss@gmail.com>
2025-10-16 16:46:50 +0000

    second one

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@
 import { useState } from "react";
 import { Dialog, DialogPanel } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
-import Mode from "./Mode"; // <-- ajouté
+import Mode from "./Mode";
 
 const navigation = [
   { name: "Product", href: "#" },
